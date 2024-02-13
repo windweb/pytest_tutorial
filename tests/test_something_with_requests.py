d617c3c1d739154ad014511bc1a9fa2f34846be1
Aidarov Kirill <github@aidarov.ru>
2024-02-13 08:59:31 +0300

    update files

--- tests/test_something_with_requests.py
+++ tests/test_something_with_requests.py
@@ -1,7 +1,7 @@
 import requests
-from jsonschema import validate
+# from jsonschema import validate
 from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
+# from src.enums.global_enums import GlobalErrorMessages
 from src.baseclasses.response import Response
 from src.schemas.post import POST_SCHEMA
 
@@ -10,7 +10,6 @@ from src.schemas.post import POST_SCHEMA
 def test_getting_posts():
     r = requests.get(url=SERVICE_URL, )
     response = Response(r)
-
     response.assert_status_code(200).validate(POST_SCHEMA)
 
     # received_posts = response.json()
